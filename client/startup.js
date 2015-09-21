f31f85a2bcc62627ac9393b11a4722bf7ba8836c
Maxim Gryazonv <lkdnvc@gmail.com>
2015-09-21 14:43:38 +0300

    login modal video instructions

--- client/startup.js
+++ client/startup.js
@@ -2,6 +2,7 @@ PNotify.prototype.options.styling = 'fontawesome';
 
 Meteor.startup(function () {
   moment.locale('ru');
+
   return TAPi18n.setLanguage('ru').done(function () {
     return _.each(Schemas, function (schema) {
       schema.i18n('formFields');
--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -1,13 +1,13 @@
 AutoForm.hooks({
   loginForm: {
     before: {
-      method: function(doc) {
+      method: function (doc) {
         Session.set('LoginAttempt', doc.password);
         doc.password = 'fakePass';
         return doc;
       }
     },
-    onError: function(type, error) {
+    onError: function (type, error) {
       console.log(error);
       if (error.error === 490) {
         return Router.go('blocked');
@@ -19,13 +19,13 @@ AutoForm.hooks({
         });
       }
     },
-    onSuccess: function(type, result) {
+    onSuccess: function (type, result) {
       var password;
       console.log(result);
       if (result) {
         password = Session.get('LoginAttempt');
         Session.set('LoginAttempt', void 0);
-        return Meteor.loginWithPassword(result, password, function(error) {
+        return Meteor.loginWithPassword(result, password, function (error) {
           if (error) {
             log.info(error);
             return new PNotify({
@@ -42,18 +42,15 @@ AutoForm.hooks({
   }
 });
 
-Template.login.rendered = function() {
+Template.login.rendered = function () {
   return log.trace('login rendered');
 };
 
-Template.login.helpers({
-  iamlogin: function() {
-    return 'iam login';
-  }
-});
-
 Template.login.events({
-  "click #login": function() {
+  "click #login": function () {
     return log.trace('click #login');
+  },
+  "click [name=loginInstructionsShow]": function () {
+    Modal.show('loginInstructionsModal');
   }
 });
