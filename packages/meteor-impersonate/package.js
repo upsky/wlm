88c359c99a535dc0c93964f6f2a6d86185b169a5
Kostya Kurilov <krizka@gmail.com>
2015-09-24 12:38:54 +0300

    add checked impersonate, which denies collection operations while impersonation

---
+++ packages/meteor-impersonate/package.js
@@ -0,0 +1,32 @@
+Package.describe({
+  name: "gwendall:impersonate",
+  summary: "Impersonate users in Meteor",
+  version: "0.2.1",
+  git: "https://github.com/gwendall/meteor-impersonate.git",
+});
+
+Package.onUse(function (api, where) {
+
+  api.use([
+    "accounts-base",
+    "reactive-var",
+    "templating",
+    "gwendall:body-events"
+  ], "client");
+
+  api.use([
+    "random",
+    "alanning:roles",
+  ]);
+
+  api.addFiles([
+    "server/lib.js"
+  ], "server");
+
+  api.addFiles([
+    "client/lib.js"
+  ], "client");
+
+  api.export("Impersonate");
+
+});
---
+++ packages/meteor-impersonate/server/methods.js
@@ -0,0 +1,34 @@
+/**
+ * Created by kriz on 24/09/15.
+ */
+
+var originalMethods = Meteor.methods;
+
+Meteor.methods = function (methods) {
+	var checkedMethods = _.mapObject(methods, function (method) {
+		return function () {
+			// save impersonation to current fiber to make it available anywhere inside method
+			if (this.connection.impersonate)
+				Fiber.current().impersonate = this.connection.impersonate;
+			method.apply(this, arguments);
+		}
+	});
+	originalMethods.call(Meteor, checkedMethods);
+};
+
+// update collection prototypes
+var proto = Meteor.Collection.prototype;
+
+['update', 'upsert', 'insert', 'remove'].forEach(function (methodName) {
+	var origMethod = origCollection[methodName] = proto[methodName];
+	proto[methodName] = function () {
+		// check impersonation here
+		var impersonate = Fiber.current().impersonate;
+		if (impersonate) {
+			var message = 'trying to call Collection.' + methodName + ' while impersonation';
+			console.log(message);
+			throw new Meteor.Error(404, message);
+		}
+		return origMethod.apply(this, arguments);
+	}
+})
