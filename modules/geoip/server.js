30d9f67f9c9eebf064868f396e808e0c4a35afaf
Anton <antolyk@mail.ru>
2015-09-29 10:19:32 +0300

    рефакторинг кода геолокации, по просьбе Кости

--- modules/geoip/server.js
+++ modules/geoip/server.js
@@ -2,18 +2,10 @@ if (Meteor.isServer) {
 
   Accounts.onLogin (function(res){
     var thisUser = res.user;
-    var checkResult = false;
-    if (thisUser.status == undefined) {
-      checkResult = true;
-    }
-    else if (thisUser.status.firstLogin == undefined) {
-      checkResult = true;
-    }
-    if (checkResult){
 
-     /* var nowIp = res.connection.clientAddress;
-       на локалке не сработает*/
-      var nowIp = "95.78.127.164";
+    if (thisUser["status.firstLogin"] == undefined){
+
+      var nowIp = res.connection.clientAddress;
       var geo = GeoIP.lookup(nowIp);
       var geoUpdate = {};
       for(var property in geo) {
