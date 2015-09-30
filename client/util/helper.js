8a654b6d94d2f4c9b37369dd6b490f22c363bac8
Maxim Gryazonv <lkdnvc@gmail.com>
2015-09-30 14:58:03 +0300

    bug fix with show qr and img max width 100%

--- client/util/helper.js
+++ client/util/helper.js
@@ -6,6 +6,5 @@ Template.registerHelper('cordovaDedect', function () {
 	return (!Meteor.isCordova ? 'c-cordova' : '');
 });
 Template.registerHelper('showQr', function () {
-	console.log('Meteor.settings.showQr', Meteor.settings.showQr);
 	return Meteor.settings.showQr;
 });
