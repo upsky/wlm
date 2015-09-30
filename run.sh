3b85b0fbc1dc71ed19cfda3c59d100da89158452
Kostya Kurilov <krizka@gmail.com>
2015-09-30 12:03:39 +0300

    Settings templates Command for run android

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HOST=wlmarket.meteor.com
+HOST=wlm.he24.ru
 SETTINGS="--settings settings.json"
 
 case $1 in
@@ -13,11 +13,17 @@ case $1 in
     ios)
         meteor run ios-device $SETTINGS --mobile-server=$HOST $2 $3 $4
     ;;
-    deploy)
+    android)
+        meteor run android-device $SETTINGS --mobile-server=$HOST $2 $3 $4
+    ;;
+    deploy-meteor)
         meteor deploy $HOST $SETTINGS
     ;;
     build)
         meteor build .out --server=$HOST --mobile-settings settings.json
+    ;;
+    deploy)
+        mupx deploy
     ;;
     *)
         echo "usage: $0 [run|ios] params" && exit 1
