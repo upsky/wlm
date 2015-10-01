809849c925f6b91740129d5ee44af5bd643baee2
Kostya Kurilov <krizka@gmail.com>
2015-10-01 16:57:33 +0300

    fix log logins

--- packages/log-logins/log-logins.js
+++ packages/log-logins/log-logins.js
@@ -1,19 +1,36 @@
-Accounts.onLogin (function(res){
+Accounts.onLogin(function (res) {
 	var thisUser = res.user;
 	var nowIp = res.connection.clientAddress;
 	var geo = GeoIP.lookup(nowIp);
+
 	var geoUpdate = {
 		ip: nowIp,
-		country: geo.country,
-		region: geo.region,
-		city: geo.city,
-		ll: geo.ll
+		createdAt: new Date()
 	};
-	if (thisUser.status == undefined ||
-		thisUser.status.firstLogin == undefined){
-		db.users.update({_id:thisUser._id},{$set:{status:{geo:geoUpdate,firstLogin: new Date()}}});
+	if (geo) {
+		_.extend(geoUpdate, {
+			country: geo.country,
+			region: geo.region,
+			city: geo.city,
+			ll: geo.ll
+		});
 	}
+
+	if (thisUser.status == undefined ||
+		thisUser.status.firstLogin == undefined) {
+		db.users.update(
+			{ _id: thisUser._id },
+			{
+				$set: {
+					status: {
+						geo: geoUpdate,
+						firstLogin: new Date()
+					}
+				}
+			}
+		);
+	}
+
 	geoUpdate.userId = thisUser._id;
-	geoUpdate.createdAt = new Date();
 	db.logLogins.insert(geoUpdate);
 });
