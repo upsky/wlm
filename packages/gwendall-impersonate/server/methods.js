d816f3cdaa2283f83ffd045b3d6b8d7b8720bc2a
Kostya Kurilov <krizka@gmail.com>
2015-10-01 16:07:12 +0300

    fix impersonation

--- packages/gwendall-impersonate/server/methods.js
+++ packages/gwendall-impersonate/server/methods.js
@@ -7,19 +7,22 @@ var Fiber = Npm.require('fibers');
 var checkImpersonation = function (impersonate, methodName) {
 	var roles = Impersonate.allowedMethods[methodName];
 	var forAll = roles === 'all';
-	var isInRole = !_.isEmpty(roles) && Roles.isInRole(impersonate.toUser, roles);
+	var isInRole = !_.isEmpty(roles) && Roles.userIsInRole(impersonate.originalUser, roles);
 	if (!forAll && !isInRole)
 		throw new Meteor.Error(403, 'method not allowed while impersonation', methodName);
 };
 
 Meteor.beforeAllMethods(function () {
 	// save impersonation to current fiber to make it available anywhere inside method
+	var methodName = this._methodName;
 	if (this.connection && this.connection.impersonate) {
+		console.log('checking impersonate for', methodName, this.connection.impersonate);
 		var impersonate = Fiber.current.impersonate = this.connection.impersonate;
 
-		checkImpersonation(impersonate, this._methodName);
-	}
+		checkImpersonation(impersonate, methodName);
 
+	}
+	console.log('impersonate ok', methodName);
 	// check impersonation for methods here
 });
 
