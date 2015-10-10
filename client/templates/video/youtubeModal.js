b04de5a15f8f02af2c42e8a87693f5d2dc6d2121
RobertNesta <azgatherzjob@gmail.ru>
2015-10-10 13:09:48 +0300

    fix

--- client/templates/video/youtubeModal.js
+++ client/templates/video/youtubeModal.js
@@ -1,22 +1,22 @@
 var currentData = Template.currentData;
 
-Template.youtubeModal.events({
-	'click [name=showVideo]': function () {
-			Session.set('video', currentData());
-			Modal.show('youtubeModalItem');
-	},
-	'click [name=changeVideo]': function () {
-			Session.set('editVideo', currentData());
-			if(db.videos.find({name: currentData()}).count() == 0 ) {
-				Session.set('route', Router.current().route.getName());
-				Router.go('addVideo');
-			} else {
-				var id = db.videos.findOne({name: currentData()})._id;
-				Session.set('route', Router.current().route.getName());
-				Router.go('editVideo', {_id: id});
-			}
-	}
-});
+// Template.youtubeModal.events({
+// 	'click [name=showVideo]': function () {
+// 			Session.set('video', currentData());
+// 			Modal.show('youtubeModalItem');
+// 	},
+// 	'click [name=changeVideo]': function () {
+// 			Session.set('editVideo', currentData());
+// 			if(db.videos.find({name: currentData()}).count() == 0 ) {
+// 				Session.set('route', Router.current().route.getName());
+// 				Router.go('addVideo');
+// 			} else {
+// 				var id = db.videos.findOne({name: currentData()})._id;
+// 				Session.set('route', Router.current().route.getName());
+// 				Router.go('editVideo', {_id: id});
+// 			}
+// 	}
+// });
 
 Template.youtubeModal.helpers({
 	isShow: function () {
